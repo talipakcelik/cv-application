8a544693cb7e268dc9f5707d88cc5450a8bc161c
Talip <talipakcelik@gmail.com>
2022-06-16 07:36:56 +0300

    add value attributes each of inputs for controlled input

--- src/App.js
+++ src/App.js
@@ -23,75 +23,88 @@ export default class App extends React.Component {
     return (
       <section>
         <header>Cv Project</header>
-        <main>
-          <h3></h3>
+        <form>
+          <h3>Personal Information</h3>
           <input
             type="text"
             placeholder="First name"
             name="firstName"
             onChange={this.handleChange}
+            value={this.state.firstName}
           ></input>
           <input
             type="text"
             placeholder="Last name"
             name="lastName"
             onChange={this.handleChange}
+            value={this.state.lastName}
           ></input>
           <input
             type="text"
             placeholder="Address"
             name="address"
             onChange={this.handleChange}
+            value={this.state.address}
           ></input>
           <input
             type="text"
             placeholder="Phone number"
             name="number"
             onChange={this.handleChange}
+            value={this.state.number}
           ></input>
           <input
             type="text"
             placeholder="Email"
             name="mail"
             onChange={this.handleChange}
+            value={this.state.mail}
           ></input>
           <input
             type="text"
             placeholder="Description"
             name="description"
             onChange={this.handleChange}
+            value={this.state.description}
           ></input>
-          <input
-            type="text"
-            placeholder="Position"
-            name="position"
-            onChange={this.handleChange}
-          ></input>
-          <input
-            type="text"
-            placeholder="Company"
-            name="company"
-            onChange={this.handleChange}
-          ></input>
-          <input
-            type="text"
-            placeholder="City"
-            name="city"
-            onChange={this.handleChange}
-          ></input>
+          <h3>Education</h3>
           <input
             type="text"
             placeholder="University"
             name="university"
             onChange={this.handleChange}
+            value={this.state.university}
           ></input>
           <input
             type="text"
             placeholder="Degree"
             name="degree"
             onChange={this.handleChange}
+            value={this.state.degree}
           ></input>
-        </main>
+          <h3>Experience</h3>
+          <input
+            type="text"
+            placeholder="Position"
+            name="position"
+            onChange={this.handleChange}
+            value={this.state.position}
+          ></input>
+          <input
+            type="text"
+            placeholder="Company"
+            name="company"
+            onChange={this.handleChange}
+            value={this.state.company}
+          ></input>
+          <input
+            type="text"
+            placeholder="City"
+            name="city"
+            onChange={this.handleChange}
+            value={this.state.city}
+          ></input>
+        </form>
       </section>
     );
   }
